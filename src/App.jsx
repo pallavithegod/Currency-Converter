a5b0f69deba31782506a04dd552fb343f7de39f0
unknown <jainpallavi.delhi@gmail.com>
2025-07-15 23:32:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <body className='bg-gray-900'>
+      <h1 className="text-3xl text-white font-bold underline">
+      Hello world!
+    </h1>
+    </body>
+    </>
+  )
+}
+
+export default App
