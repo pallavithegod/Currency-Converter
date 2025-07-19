ae5198d1d23dc0f1cfe5ab233bd9d1c4cfa200a6
unknown <jainpallavi.delhi@gmail.com>
2025-07-19 22:36:24 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  // delete -  function to be exported
 
   return (
     <>
     <body className='bg-gray-900'>
       <h1 className="text-3xl text-white font-bold">
-      CURRENCY CONVERTER
+      CURRENCY CONVERTER  
     </h1>
     </body>
     </>
